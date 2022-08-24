a13de5f164ad9e9151081b547d50fd0a5346a18d
Piyadhars <89064629+Piyadhars@users.noreply.github.com>
2022-08-24 11:19:20 +0530

    Update Coing.java

--- Coing.java
+++ Coing.java
@@ -7,20 +7,28 @@ public class Coing {
         System.out.println("Enter length of the array: ");
         int n=in.nextInt();
         int a[]=new int[n];
+        System.out.println("Enter elements of the array: ");
         for(int i=0;i<n;i++)
         {
             a[i]=in.nextInt();
         }
         System.out.println("Enter sum: ");
         int sum=in.nextInt();
+        int r=sum;
         Arrays.sort(a);
         int count=0;
         for(int i=a.length-1;i>=0;i--)
         {
-            if(sum>=a[i])
+            while(r>=a[i])
             {
-                sum=sum-a[i];
+                r=r-a[i];
                 count+=1;
+            }
+            if(r!=0 && a[i-1]>sum)
+            {
+                r=sum;
+                count=0;
+                continue;
             }
         }
         System.out.println("Minimum coins: "+count);
